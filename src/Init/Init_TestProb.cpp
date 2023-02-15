71489164e8c06f02438e7c5efdc37339499a0321
Nai-Chieh Lin <daniellin456@gmail.com>
2023-02-15 13:33:01 +0800

    <feat> add new molecular cloud collapse problem

--- src/Init/Init_TestProb.cpp
+++ src/Init/Init_TestProb.cpp
@@ -25,7 +25,7 @@ void Init_TestProb_Hydro_BarredPot();
 void Init_TestProb_Hydro_ParticleTest();
 void Init_TestProb_Hydro_CDM_LSS();
 void Init_TestProb_Hydro_Zeldovich();
-
+void Init_TestProb_Hydro_MolecularCloudCollapse();
 void Init_TestProb_ELBDM_ExtPot();
 
 
@@ -73,7 +73,7 @@ void Init_TestProb()
       case TESTPROB_HYDRO_PARTICLE_TEST :                Init_TestProb_Hydro_ParticleTest();                break;
       case TESTPROB_HYDRO_CDM_LSS :                      Init_TestProb_Hydro_CDM_LSS();                     break;
       case TESTPROB_HYDRO_ZELDOVICH :                    Init_TestProb_Hydro_Zeldovich();                   break;
-
+      case TESTPROB_HYDRO_MOLECULAR_CLOUD_COLLAPSE :     Init_TestProb_Hydro_MolecularCloudCollapse();      break;
       case TESTPROB_ELBDM_EXTPOT :                       Init_TestProb_ELBDM_ExtPot();                      break;
 
       default: Aux_Error( ERROR_INFO, "unsupported TESTPROB_ID (%d) !!\n", TESTPROB_ID );
